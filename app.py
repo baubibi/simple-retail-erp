a2312f09ef31a8ecb456f8ebb87bb72ed3caa458
baubibi <baubibi@gmail.com>
2025-07-21 15:08:04 +0800

    Add basic user management

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from database import init_db, db_session
-from models import Product
+from models import Product, User
 from sqlalchemy.exc import IntegrityError
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 init_db()
@@ -23,6 +24,44 @@ def add_product():
     except IntegrityError:
         db_session.rollback()
     return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User(username=username)
+        user.set_password(password)
+        try:
+            db_session.add(user)
+            db_session.commit()
+            return redirect(url_for("login"))
+        except IntegrityError:
+            db_session.rollback()
+            error = "使用者名稱已存在"
+            return render_template("register.html", error=error)
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = db_session.query(User).filter_by(username=username).first()
+        if user and user.check_password(password):
+            return redirect(url_for("list_users"))
+        else:
+            error = "帳號或密碼錯誤"
+    return render_template("login.html", error=error)
+
+
+@app.route("/users")
+def list_users():
+    users = db_session.query(User).all()
+    return render_template("users.html", users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
